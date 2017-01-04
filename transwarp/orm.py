e328341b41bfedcaffff7a3bad3acf7ace41ff4b
ruan lianjie <ruan.lj@foxmail.com>
2017-01-04 20:36:16 +0800

    add commit of day-03

--- transwarp/orm.py
+++ transwarp/orm.py
@@ -5,6 +5,7 @@ __author__ = 'Michael Liao'
 
 '''
 Database operation module. This module is independent with web module.
+orm的实现，里面使用了很多元编程的技巧，非常有价值。
 '''
 
 import time, logging
@@ -12,22 +13,25 @@ import time, logging
 import db
 
 class Field(object):
+    """用来描述一个字段的类型，并且提供该字段ddl，
+    下面的各个Field的子类，也是类似
+    """
 
-    _count = 0
+    _count = 0  # 记录字段个数，并且为一个表的字段提供排序
 
     def __init__(self, **kw):
-        self.name = kw.get('name', None)
-        self._default = kw.get('default', None)
-        self.primary_key = kw.get('primary_key', False)
-        self.nullable = kw.get('nullable', False)
-        self.updatable = kw.get('updatable', True)
+        self.name = kw.get('name', None)  # 字段名
+        self._default = kw.get('default', None)  # 默认值
+        self.primary_key = kw.get('primary_key', False)  # 是否为主键
+        self.nullable = kw.get('nullable', False)  # 是否可空
+        self.updatable = kw.get('updatable', True)  # 是否可更新
         self.insertable = kw.get('insertable', True)
         self.ddl = kw.get('ddl', '')
         self._order = Field._count
         Field._count = Field._count + 1
 
     @property
-    def default(self):
+    def default(self):  # 默认值支持可调用对象
         d = self._default
         return d() if callable(d) else d
 
@@ -118,13 +122,22 @@ def _gen_sql(table_name, mappings):
 class ModelMetaclass(type):
     '''
     Metaclass for model objects.
+
+    整个orm的精华所在
     '''
     def __new__(cls, name, bases, attrs):
+        """
+        name: 类名，str
+        bases： 基类， list
+        attrs： 类属性， dict
+        编写元类，基本上就是写__new__方法，__new__需要返回一个类对象
+        """
         # skip base Model class:
+        # 只有用户自定义的model才需要通过元类的加工
         if name=='Model':
             return type.__new__(cls, name, bases, attrs)
 
-        # store all subclasses info:
+        # store all subclasses info， 记录子类信息，还没发现什么地方用到cls.subclasses，
         if not hasattr(cls, 'subclasses'):
             cls.subclasses = {}
         if not name in cls.subclasses:
@@ -133,21 +146,28 @@ class ModelMetaclass(type):
             logging.warning('Redefine class: %s' % name)
 
         logging.info('Scan ORMapping %s...' % name)
-        mappings = dict()
-        primary_key = None
+        mappings = dict()  # 记录类属性与表的字段的对应关系，并不是每个类属性都是表的字段
+        primary_key = None  # 标记一个表的主键
+
+        # >>> class User(Model):
+        # ...     id = IntegerField(primary_key=True)
+        # ...     name = StringField()
+        # ...
+        # attrs 类似 {id: IntegerField, name: StringField}
+        # k 就是字段名， v就是字段类型
         for k, v in attrs.iteritems():
             if isinstance(v, Field):
-                if not v.name:
+                if not v.name:  # 若一个字段未指定字段名，就用类属性名
                     v.name = k
                 logging.info('Found mapping: %s => %s' % (k, v))
                 # check duplicate primary key:
                 if v.primary_key:
-                    if primary_key:
+                    if primary_key:  # 主键唯一， 以下三个校验都是为了生成表的ddl
                         raise TypeError('Cannot define more than 1 primary key in class: %s' % name)
-                    if v.updatable:
+                    if v.updatable:  # 字段是否可改
                         logging.warning('NOTE: change primary key to non-updatable.')
                         v.updatable = False
-                    if v.nullable:
+                    if v.nullable:  # 字段是否可设置为空
                         logging.warning('NOTE: change primary key to non-nullable.')
                         v.nullable = False
                     primary_key = v
@@ -157,12 +177,12 @@ class ModelMetaclass(type):
             raise TypeError('Primary key not defined in class: %s' % name)
         for k in mappings.iterkeys():
             attrs.pop(k)
-        if not '__table__' in attrs:
+        if not '__table__' in attrs:  # 默认用小写的类名作为表名
             attrs['__table__'] = name.lower()
         attrs['__mappings__'] = mappings
         attrs['__primary_key__'] = primary_key
-        attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)
-        for trigger in _triggers:
+        attrs['__sql__'] = lambda self: _gen_sql(attrs['__table__'], mappings)  # 生成建表的ddl语句
+        for trigger in _triggers:  # 可能是预留的接口，用于控制['pre_insert', 'pre_update', 'pre_delete']方法
             if not trigger in attrs:
                 attrs[trigger] = None
         return type.__new__(cls, name, bases, attrs)
@@ -170,7 +190,8 @@ class ModelMetaclass(type):
 class Model(dict):
     '''
     Base class for ORM.
-
+    Model是python对象和mysql表的对应关系，Model就是代表数据表，Model的实例就代表记录
+    类属性名代表字段名，类属性值说明这个字段的限制条件
     >>> class User(Model):
     ...     id = IntegerField(primary_key=True)
     ...     name = StringField()
@@ -214,12 +235,12 @@ class Model(dict):
       primary key(`id`)
     );
     '''
-    __metaclass__ = ModelMetaclass
+    __metaclass__ = ModelMetaclass  # 设置元类
 
-    def __init__(self, **kw):
+    def __init__(self, **kw):  # 初始化实例时，调用dict的init方法
         super(Model, self).__init__(**kw)
 
-    def __getattr__(self, key):
+    def __getattr__(self, key):  # 这样可以通过属性访问对应的key，db.py中有相同写法
         try:
             return self[key]
         except KeyError:
@@ -228,6 +249,7 @@ class Model(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
+    # 下面的这些类方法，就是对model对应的表进行增删查改的操作
     @classmethod
     def get(cls, pk):
         '''
@@ -239,7 +261,7 @@ class Model(dict):
     @classmethod
     def find_first(cls, where, *args):
         '''
-        Find by where clause and return one result. If multiple results found, 
+        Find by where clause and return one result. If multiple results found,
         only the first one returned. If no result found, return None.
         '''
         d = db.select_one('select * from %s %s' % (cls.__table__, where), *args)
@@ -276,11 +298,12 @@ class Model(dict):
         return db.select_int('select count(`%s`) from `%s` %s' % (cls.__primary_key__.name, cls.__table__, where), *args)
 
     def update(self):
-        self.pre_update and self.pre_update()
-        L = []
-        args = []
+        """update是实例方法"""
+        self.pre_update and self.pre_update()  # 预留接口
+        L = []  # 类似 ['`id`=?', '`name`=?']
+        args = [] # 类似 [1, 'shabi']
         for k, v in self.__mappings__.iteritems():
-            if v.updatable:
+            if v.updatable:  # v是Field实例代表字段，也就是这个字段可更新
                 if hasattr(self, k):
                     arg = getattr(self, k)
                 else:
@@ -288,12 +311,12 @@ class Model(dict):
                     setattr(self, k, arg)
                 L.append('`%s`=?' % k)
                 args.append(arg)
-        pk = self.__primary_key__.name
-        args.append(getattr(self, pk))
+        pk = self.__primary_key__.name  # 主键名
+        args.append(getattr(self, pk))  # 最后一个参数是主键的值
         db.update('update `%s` set %s where %s=?' % (self.__table__, ','.join(L), pk), *args)
         return self
 
-    def delete(self):
+    def delete(self): # 预留接口
         self.pre_delete and self.pre_delete()
         pk = self.__primary_key__.name
         args = (getattr(self, pk), )
